eae7ddb8c1e34210e1e219733a5b60bc4239493c
Yomar Miranda <mirandayomar@gmail.com>
2018-12-12 18:05:57 -0500

    Create main components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoFooterComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
