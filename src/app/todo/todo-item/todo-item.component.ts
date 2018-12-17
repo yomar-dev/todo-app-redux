ad9e389a41f2cf743cbab247da4ccdda2d96094b
Yomar Miranda <mirandayomar@gmail.com>
2018-12-17 15:06:38 -0500

    46 - Control all todo items.

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
+  @Input() todo: Todo;
+  @ViewChild('txtTodo') txtTodo: ElementRef;
+
+  checkField: FormControl;
+  txtInput: FormControl;
+
+  editing: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkField = new FormControl(this.todo.completed);
+    this.txtInput = new FormControl(this.todo.text, Validators.required);
+  }
+
+  edit() {
+    this.editing = true;
+    setTimeout(() => {
+      this.txtTodo.nativeElement.select();
+    }, 1);
+  }
+
+  finishedEdition() {
+    this.editing = false;
   }
 
 }
--- src/app/todo/todo.reducer.ts
+++ src/app/todo/todo.reducer.ts
@@ -4,6 +4,8 @@ import { Todo } from './models/todo.model';
 const walk = new Todo('Walk');
 const run = new Todo('Run');
 
+walk.completed = true;
+
 const initialState: Todo[] = [walk, run];
 
 export function todoReducer(state = initialState, action: fromTodo.Actions): Todo[] {
